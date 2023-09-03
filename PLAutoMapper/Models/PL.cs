bac3ed4b98aa78e6a1889421ea1ab7d2ba9bf12e
준석 손 <thstjr0487@naver.com>
2023-09-04 00:47:30 +0900

    1111

---
+++ PLAutoMapper/Models/PL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLAutoMapper.Models
+{
+    public class PL
+    {
+        public int Id { get; set; }
+        public string PLName { get; set; }
+        public string Site { get; set; }
+    }
+}
